68d3a4ba6ecb4e60e8ba7103c87d025161c62290
yuanjie <1046452920@qq.com>
2018-07-11 16:34:08 +0800

    00000

---
+++ LeoJLovePod/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  LeoJLovePod
+//
+//  Created by mac on 2018/7/11.
+//  Copyright © 2018年 Yj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.yellow
+        self.title = "second cocoapod"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
